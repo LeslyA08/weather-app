06e4413b79775bbf50e65b6788e194986037269b
Lesly Alvarez <leslyalvarez08@gmail.com>
2022-08-30 11:22:27 -0500

    checked if API is connected, added what information I want on the site

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
 import './App.css';
 import {useState, useEffect} from 'react';
-import Button from "./components/Button/Button"
-import WeatherContainer from "./components/WeatherContainer/WeatherContainer"
 
 
 function App() {
   const [weather, setWeather] = useState([]);
+  const [location, setLocation] = useState('')
 
   const getWeather = () => {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=9636a0d670bb028a34afd3c82ff192b5")
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=9636a0d670bb028a34afd3c82ff192b5`)
     .then((response) => {
       return response.json()
     })
     .then(data => {
-      setWeather(data.response)
+      setWeather(data)
     })
   };
+  useEffect(() => {
+    getWeather()
+  }, []);
 
 
   return (
     <>
       <div>
-        <h1 className="name">Dallas</h1>
+        <h1 className="name">{weather.name}</h1>
         <p className="temperature">60 degrees F</p>
         <p className="description">Clouds</p>
+        <p>Feels Like</p>
         <p className="feelsLike">65 degrees F</p>
+        <p>Humidity</p>
         <p className="humidity">20%</p>
+        <p>Wind Speed</p>
         <p className="wind"> 12 MPH</p>
       </div>
     </>
